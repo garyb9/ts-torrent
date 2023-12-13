47bfd48f8cd2540a0a473b19f2d8f584c61a1fcf
garyb9 <gberkinfand@gmail.com>
2023-12-14 01:10:35 +0200

    adding recursive nested json buffer converter to string

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import bencodec from 'bencodec';
+import { convertBuffersToStrings } from './utils'
 
 const torrentPath: string = path.join(__dirname, '..', 'public', '924F1C9B89F7543DBBA5CA0E30A5CF4F2E112360.torrent');
 const torrent: Buffer = fs.readFileSync(torrentPath);
 const decoded = bencodec.decode(torrent);
-console.log(decoded);
+console.log(convertBuffersToStrings(decoded));
